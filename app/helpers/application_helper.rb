26aa1f932c783d0abf90386e48f73ef0c4b5a996
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-07-19 16:50:09 -0700

    Finish discussion index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
     def login_button_helper
         if current_user 
-            (link_to "Account", edit_user_registration_path, class:'button is-link login-helper-button') +
-            (link_to "Log out", destroy_user_session_path, class: "button is-danger login-helper-button", method: :delete)
+            (link_to "Account", edit_user_registration_path, class:'button is-link login-helper-button is-outlined') +
+            (link_to "Log out", destroy_user_session_path, class: "button is-danger login-helper-button is-outlined", method: :delete)
         else 
-            (link_to "Log in", new_user_session_path, class:'button is-info login-helper-button') +
-            (link_to "<strong>Sign up</strong>".html_safe, new_user_registration_path, class:'button is-info is-outlined login-helper-button')
+            (link_to "Log in", new_user_session_path, class:'button is-info login-helper-button is-outlined') +
+            (link_to "Sign up", new_user_registration_path, class:'button is-info login-helper-button')
         end    
     end
 end
