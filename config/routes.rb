f34189cfdb6b80d5d2d3f563b66f79bf99c2ba17
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-07-20 15:28:28 -0700

    refine styling

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :channels
   get 'channel/title'
   devise_for :users
-  root 'home#index'
+  root 'channels#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
