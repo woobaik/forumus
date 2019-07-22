2b43f30adc5a43151bbc384348df44a7f9d36134
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-07-22 13:06:03 -0700

    styling app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,8 @@ module ApplicationHelper
             (link_to "Account", edit_user_registration_path, class:'button login-helper-button is-outlined login-helper-acct') +
             (link_to "Log out", destroy_user_session_path, class: "button login-helper-button is-outlined login-helper-logout", method: :delete)
         else 
-            (link_to "Log in", new_user_session_path, class:'button login-helper-button is-outlined') +
-            (link_to "Sign up", new_user_registration_path, class:'button login-helper-button')
+            (link_to "Log in", new_user_session_path, class:'button login-helper-button is-outlined login-helper-acct') +
+            (link_to "Sign up", new_user_registration_path, class:'button login-helper-button login-helper-logout')
         end    
     end
 end
