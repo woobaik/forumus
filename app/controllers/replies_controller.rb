2baae1a6655ab8cae6d0034ace84bade3ec6c8cb
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-07-25 03:57:21 -0700

    Working on comments progress

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,6 +1,14 @@
 class RepliesController < ApplicationController
     def create
         @discussion = Discussion.find(params[:discussion_id])
-        @reply = @discussion.replies.create(params[:reply].permit(:reply, :user))        
+        @reply = @discussion.replies.create(params.require(:reply).permit(:reply))        
+        @reply.user_id = current_user.id
+        respond_to do |format| 
+            if @reply.save
+                format.js { render :create }
+            else
+                format.html { render 'discussion#show' }
+            end
+        end
     end
 end
